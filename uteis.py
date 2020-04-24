79ef58bd8a5cc0e7f42411f9df7c0803fa4595d1
Your Name <you@example.com>
2020-04-24 19:06:19 -0300

    Add docstrings

--- uteis.py
+++ uteis.py
@@ -1,9 +1,11 @@
+"""Module with functions connect"""
 import paramiko
 
 SSH = paramiko.SSHClient()
 SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 def lookip(ip_number, login_name, password_number, shell_command):
+    """ Function connect commands """
     SSH.connect(ip_number, username=login_name, password=password_number)
     SSH.exec_command(shell_command)
     SSH.close()
