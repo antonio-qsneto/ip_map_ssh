0c56dadf1c11a7e7f63a8e31fa903c3c446abbee
Your Name <you@example.com>
2020-04-24 17:27:46 -0300

    update de uteis function

--- uteis.py
+++ uteis.py
@@ -1,24 +1,9 @@
 import paramiko
-import socket
-import os
-import nmap
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-nm = nmap.PortScanner()
-hosts = []
+SSH = paramiko.SSHClient()
+SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-def lookip(ip, port):
-    nm.scan(ip, port)
-    for host in nm.all_hosts():
-        hosts.append(host)
-    return hosts
-
-def conect():
-    for devices in lookip('192.168.0.0/24', '22-443'):
-        try:
-            ssh.connect(devices, username='qwerty', password='init4289')
-            print("conectado")
-            ssh.exec_command('eject -r')
-        except:
-            continue
+def lookip(ip_number, login_name, password_number, shell_command):
+    SSH.connect(ip_number, username=login_name, password=password_number)
+    SSH.exec_command(shell_command)
+    SSH.close()
